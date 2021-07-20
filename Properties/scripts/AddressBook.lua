e9f7573fde9887debaa529f4ae31e4e9819d3a47
Mikolaj Rdzanek <mikolaj.rdzanek@sick.de>
2021-07-20 14:10:20 +0200

    'Update'

--- Properties/scripts/AddressBook.lua
+++ Properties/scripts/AddressBook.lua
@@ -1,21 +1,4 @@
---[[----------------------------------------------------------------------------
 
-  Application Name: Properties
-
-  Description:
-  Introduction to creating CROWN data structures with Properties for handling complex data.
-
-  In this sample a new data type, called "Address", is declared via App Properties dialog.
-  This data type has "Properties", which are basically a pair of get()/set()-Functions
-  which are automatically implemented at declaration point. The set() function stores
-  data, the get() function retrieves it later. It can be compared to declaration of
-  structs in C. Main reason for Crowns with Properties is to provide new compound
-  data types for inter-app data exchange. This file offers functionality of an
-  address book, where "Address" data items can be stored (a Lua list of Address items).
-  The file "AddressBookClient.lua" then uses the AddressBook as client to add some
-  Address instances and to print them out.
-
- ------------------------------------------------------------------------------]]
 --Start of Global Scope---------------------------------------------------------
 local client = require('AddressBookClient')
 
--- readme.md
+++ readme.md
@@ -1,5 +1,6 @@
 ## Properties
-Introduction to creating CROWN data structures with Properties for handling complex data
+Introduction to creating CROWN data structures with Properties for handling complex data.
+
 ### Description
 In this sample a new data type, called "Address", is declared via App Properties dialog.
 This data type has "Properties", which are basically a pair of get()/set()-Functions
@@ -9,7 +10,11 @@ structs in C. Main reason for Crowns with Properties is to provide new compound
 data types for inter-App data exchange. This file offers functionality of an
 address book, where "Address" data items can be stored (a Lua list of Address items).
 The file "AddressBookClient.lua" then uses the AddressBook as client to add some
-Address instances and to print them out
+Address instances and to print them out.
+
+### HOw To Run:
+The app can be run with any AppSpace device or the emulator.
+The results of the program logic will be printed to the console.
 
 ### Topics
 Programming-Pattern, Sample, SICK-AppSpace
